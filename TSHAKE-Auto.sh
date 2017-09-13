9a891c2f46e9216144ca1e2a0f40da9d06bbf1ef
ahmed1998aa <30708899+ahmed1998aa@users.noreply.github.com>
2017-09-14 00:11:05 +0300

    Update TSHAKE-Auto.sh

--- TSHAKE-Auto.sh
+++ TSHAKE-Auto.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./TSHAKE.sh
-curl "https://api.telegram.org/bot[420405135:AAE58wCwQiHUcBer0VByMoV0YXWrP_repH4]/sendmessage" -F "chat_id=138881125" -F "text=#NEWCRASH-#TshAkE-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot[420405135:AAGmhHgaVPSoUaegS9k4yLPwZPyvOXoomaM]/sendmessage" -F "chat_id=138881125" -F "text=#NEWCRASH-#TshAkE-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
